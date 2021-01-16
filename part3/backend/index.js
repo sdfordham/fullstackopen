c61e0c352564515872cda557a16f0f7875f2eb7c
Stiofáin <stiofainf@gmail.com>
2021-01-16 18:32:11 +0000

    Ex. 3.2.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -12,6 +12,18 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const info = "<div>\
+                    <p>Phonebook has info for " +  persons.length + " people.</p>\
+                    <p>" + Date().toLocaleString() + "</p>\
+                  </div>"
+    response.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
