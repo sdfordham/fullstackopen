31decfaa5175cd580ab809f82bad4af64ca7a09b
Stiofáin <stiofainf@gmail.com>
2021-01-30 13:10:49 +0000

    Ex. 3.13, 3.14.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 var morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -26,35 +28,17 @@ app.use(
   })
 )
 
-let persons = [
-    { id: 1, name: 'Arto Hellas', number: '040-123456' },
-    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -70,22 +54,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const idx = persons.find(person => person.name == body.name)
-  if (idx !== undefined) {
-    return response.status(400).json({ 
-      error: 'name must be unique.' 
-    })
-  }
-
-  const person = {
-    id: getRandomInt(100000),
+  const person = new Person({
     name: body.name,
-    number: body.number,
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -102,7 +78,7 @@ app.get('/info', (request, response) => {
     response.send(info)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
