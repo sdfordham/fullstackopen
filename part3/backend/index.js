16ea29cd8b5a4678e688f749daf47b1b87abe219
Stiofáin <stiofainf@gmail.com>
2021-01-30 16:55:18 +0000

    Ex. 3.16.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -77,8 +77,7 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  }
   next(error)
 }
 
