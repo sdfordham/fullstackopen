36f837c20bbedb887acf9c4e58e058ad6fe0d205
Stiofáin <stiofainf@gmail.com>
2021-10-17 23:01:44 +0100

    Ex. 7.4.

--- part7/routed-anecdotes-main/src/App.js
+++ part7/routed-anecdotes-main/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {
   Switch, Route, Link, useRouteMatch
 } from "react-router-dom"
+import { useField } from './hooks/index'
 
 const Notification = ({ notification }) => {
   return (
@@ -57,19 +58,19 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    props.setNotification('a new anecdote ' + content + ' created!')
+    props.setNotification('a new anecdote ' + content.value + ' created!')
     setTimeout(() => {props.setNotification(null)}, 10000)
   }
 
@@ -79,15 +80,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -142,8 +143,6 @@ const App = () => {
   const anecdote = match 
     ? anecdotes.find(anecdote => anecdote.id === match.params.id)
     : null
-
-  console.log("Match:", match)
 
   return (
     <div>
