645fb369d0145199956d1c83ce83787a9c469434
Stiofain <stiofainf@gmail.com>
2021-11-01 21:59:41 +0000

    Lint and add semis

--- part9/calculateBmi.ts
+++ part9/calculateBmi.ts
@@ -1,14 +1,14 @@
 const calculateBmi = (height: number, weight: number): string => {
-  const idx = weight / ( (height / 100) ** 2)
+  const idx = weight / ( (height / 100) ** 2);
   if (idx <= 18.4) {
-    return "Underweight"
+    return 'Underweight';
   } else if (idx <= 24.9) {
-    return "Normal range"
+    return 'Normal range';
   } else if (idx <= 29.9) {
-    return "Overweight"
+    return 'Overweight';
   } else {
-    return "Obese"
+    return 'Obese';
   }
-}
+};
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 74));
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,4 +1,4 @@
-const target = 2
+const target = 2;
 
 interface exerciseSummary {
     periodLength: number,
@@ -11,23 +11,23 @@ interface exerciseSummary {
 }
 
 const calculateExercises = (dailyHours: Array<number>): exerciseSummary => {
-    const periodLength = dailyHours.length;
-    const trainingDays = dailyHours.reduce( (a, b) => b > 0 ? a + 1 : a, 0);
-    const total = dailyHours.reduce( (a, b) => a + b, 0);
-    const success = total >= target ? true : false
-    const rating = success ? 3 : 1
-    const ratingDescription = success ? 'nice one buddy' : 'not too bad but could be better' 
-    const average = total / periodLength;
+  const periodLength = dailyHours.length;
+  const trainingDays = dailyHours.reduce( (a, b) => b > 0 ? a + 1 : a, 0);
+  const total = dailyHours.reduce( (a, b) => a + b, 0);
+  const success = total >= target ? true : false;
+  const rating = success ? 3 : 1;
+  const ratingDescription = success ? 'nice one buddy' : 'not too bad but could be better';
+  const average = total / periodLength;
     
-    return {
-        periodLength,
-        trainingDays,
-        success,
-        rating,
-        ratingDescription,
-        target,
-        average
-    }
-}
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  };
+};
 
-console.log(calculateExercises([1, 0, 0, 0.5, 0, 0, 0]))
+console.log(calculateExercises([1, 0, 0, 0.5, 0, 0, 0]));
