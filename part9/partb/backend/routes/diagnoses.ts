0d00d180df63c03ce91e9807f204de63c1457842
Stiofain <stiofainf@gmail.com>
2021-11-07 12:44:29 +0000

    Ex. 9.10.

---
+++ part9/partb/backend/routes/diagnoses.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const diagnosesRouter = express.Router();
+import diagnosesData from './json/diagnoses.json';
+import { Diagnoses } from '../types';
+
+const diagnoses: Array<Diagnoses> = diagnosesData;
+
+diagnosesRouter.get('/', async (_request, response) => {
+  response.json(diagnoses);
+});
+
+module.exports = diagnosesRouter;
