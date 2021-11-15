50a2b2264c460631cd7712f90b2f204c3d00c2e1
Stiofáin <stiofainf@gmail.com>
2021-11-15 21:56:13 +0000

    Ex. 9.15.

--- part9/partd/my-app/src/App.tsx
+++ part9/partd/my-app/src/App.tsx
@@ -8,9 +8,108 @@ const Header = (props: HeaderProps): JSX.Element => {
   return <h1>{props.name}</h1>
 }
 
-interface CoursePart{
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartBaseWithDesc {
+  name: string;
+  exerciseCount: number;
+  type: string;
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartBaseWithDesc {
+  type: "normal";
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBaseWithDesc {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecial extends CoursePartBaseWithDesc {
+  type: "special",
+  requirements: string[]
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecial;
+
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the leisured course part",
+    type: "normal"
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the harded course part",
+    type: "normal"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission"
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    type: "special"
+  }
+]
+
+const Part = (props: CoursePart): JSX.Element => {
+  switch (props.type) {
+    case "normal":
+      return (
+        <div>
+          <h3>{props.name} {props.exerciseCount}</h3>
+          <p><i>{props.description}</i></p>
+        </div>
+      )
+    case "groupProject":
+      return (
+        <div>
+          <h3>{props.name} {props.exerciseCount}</h3>
+          <p>Project exercises {props.groupProjectCount}</p>
+        </div>
+      )
+    case "submission":
+      return (
+        <div>
+          <h3>{props.name} {props.exerciseCount}</h3>
+          <p><i>{props.description}</i></p>
+          <p>Submit to {props.exerciseSubmissionLink}</p>
+        </div>
+      )
+    case "special":
+      return (
+        <div>
+          <h3>{props.name} {props.exerciseCount}</h3>
+          <p><i>{props.description}</i></p>
+          <p>Required skills: {props.requirements.join(', ')}</p>
+        </div>
+      )
+  }
 }
 
 interface ContentProps {
@@ -21,7 +120,7 @@ const Contents = (props: ContentProps): JSX.Element => {
   return (
     <div>
       {props.parts.map(part => {
-          return <p>{part.name} {part.exerciseCount}</p>
+          return <Part {...part} />
       })}
     </div>
   )
@@ -41,26 +140,12 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName: string = "Half Stack application development";
-  const courseParts: Array<CoursePart> = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
 
   return (
     <div>
       <Header name={courseName} />
       <Contents parts={courseParts} />
-      <Total total={courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}/>
+      <Total total={courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)} />
     </div>
   );
 };
